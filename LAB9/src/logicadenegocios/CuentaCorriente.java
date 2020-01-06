018e2b25c88b6108b0e8d4115e2663c2acc4a806
Óscar Trejos Esquivel <oscarandrestrejos2@gmail.com>
2020-01-06 13:56:02 -0600

    cuenta corriente

--- LAB9/src/logicadenegocios/CuentaCorriente.java
+++ LAB9/src/logicadenegocios/CuentaCorriente.java
@@ -6,6 +6,81 @@
  */
 package logicadenegocios;
 
-public class CuentaCorriente {
+import java.util.Calendar;
+
+public class CuentaCorriente extends Cuenta {
+  
+  private static int scantCuentas = 0;
+  private int operacionesExentas;
+  private double cuotaOperaciones;
+  
+  
+  /**
+   * Contructor para la clase CuentaCorriente
+   * @param pDueño
+   * @param pMonto 
+   */
+  public  CuentaCorriente(Cliente pDueño, double pMonto) {
+    super ("Cuenta Corriente ",++scantCuentas, pDueño,pMonto);
+    setOperacionesExentas(30);
+    setCuotaOperaciones(30);
+  }
+  
+  
+  /**
+   * Constructor para la clase CunetaCorriente.
+   * @param pDueño
+   * @param pMonto
+   * @param pOperExentas
+   * @param pCuotaOper 
+   */
+  public  CuentaCorriente(Cliente pDueño, double pMonto,
+  int pOperExentas, double pCuotaOper) {
+    super ("Cuenta Corriente ",++scantCuentas, pDueño,pMonto);
+    setOperacionesExentas(pOperExentas);
+    setCuotaOperaciones(pCuotaOper);
+  }
+  
+  
+  public void setOperacionesExentas(int pOperacionesExentas){
+    this.operacionesExentas = pOperacionesExentas;  
+  }
+  
+  public int getOperacionesExentas(){
+    return this.operacionesExentas ;  
+  }
+  
+  public void setCuotaOperaciones(double pCuotaOperaciones){
+    this.cuotaOperaciones = pCuotaOperaciones;  
+  } 
+  
+  public double getCuotaOperaciones(){
+    return this.cuotaOperaciones ;  
+  }
+  
+  /**
+   * 
+   * @return El monto que se cobró en el mes.
+   */
+  public String cobrarComisiones(){
+    String msg = " ";
+    double operacNoExentas;
+    double monto;
+    Calendar calendario = Calendar.getInstance();
+    int dia = calendario.get(Calendar.DAY_OF_MONTH);
+    if (dia == 1){
+      operacNoExentas = numOperaciones - getCuotaOperaciones();
+      if(operacNoExentas > 0){
+        monto = getCuotaOperaciones() * operacNoExentas;
+        retirar(monto);
+        msg = "Se cobró por comisión: "+ monto;
+      }
+      numOperaciones = 0;
+      
+    }
+    return msg;
+  }
+    
     
 }
+  
